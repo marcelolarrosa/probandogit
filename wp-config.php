a702d1abf6c28a5bb9d1ce6e55b2c0f76fd41e44
Marcelo <marcelolarrosaweb@gmail.com>
2021-05-22 13:55:53 -0300

    configg passss

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wptestinDB0qen0');
 define( 'DB_USER', 'wptestinDB0qen0');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'contra1111');
+define( 'DB_PASSWORD', 't3CIQWciqw');
 
 /** MySQL hostname */
 define( 'DB_HOST', '127.0.0.1');
